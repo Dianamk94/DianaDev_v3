81ffe3d9e4588e7537e3ef2468fc1f155b2b0966
Diana <arq.dianamendez94@gmail.com>
2026-05-06 16:34:52 -0500

    Seccion Proyectos, contacto y footer

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-import 'boxicons'
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
